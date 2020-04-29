5c61438dc069c6f80746419b5a002a29b0d18a76
hunlongyu <hunlongyu@gmail.com>
2020-04-29 09:21:40 +0800

    80

--- vue.config.js
+++ vue.config.js
@@ -7,21 +7,32 @@ module.exports = {
     electronBuilder: {
       nodeIntegration: true,
       builderOptions: {
+        appId: 'com.electron.${name}',
         nsis: {
           oneClick: false,
           allowToChangeInstallationDirectory: true
         },
-        copyright: 'Copyright @ 2020 Hunlongyu',
-        artifactName: 'P-Test_Setup_${version}.${ext}',
+        copyright: 'Copyright @ 2020 ${authore}',
+        artifactName: 'PTest-${cpu}-${version}.${ext}',
+        productName: 'pTest-${version}',
+        mac: {
+          icon: 'build/icons/icon.icons',
+          category: 'public.app-category.developer-tools',
+          target: 'default'
+        },
         win: {
-          target: {
-            target: 'nsis',
-            arch: [
-              'x64',
-              'ia32'
-            ]
-          }
-        }
+          icon: 'build/icons/icon.ico',
+          target: [ 'nsis', 'portable', 'zip' ]
+        },
+        linux: {
+          icon: 'build/icons',
+          category: 'Development'
+        },
+        publish: [{
+          'provider': 'github',
+          'owner': 'Hunlongyu',
+          'repo': 'p-test'
+        }]
       }
     }
   }
