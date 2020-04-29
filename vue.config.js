73c4131d202afe1b3491ffb40133e407dd18e77f
hunlongyu <hunlongyu@gmail.com>
2020-04-29 09:27:43 +0800

    81

--- vue.config.js
+++ vue.config.js
@@ -15,19 +15,6 @@ module.exports = {
         copyright: 'Copyright @ 2020 ${authore}',
         artifactName: 'PTest-${cpu}-${version}.${ext}',
         productName: 'pTest-${version}',
-        mac: {
-          icon: 'build/icons/icon.icons',
-          category: 'public.app-category.developer-tools',
-          target: 'default'
-        },
-        win: {
-          icon: 'build/icons/icon.ico',
-          target: [ 'nsis', 'portable', 'zip' ]
-        },
-        linux: {
-          icon: 'build/icons',
-          category: 'Development'
-        },
         publish: [{
           'provider': 'github',
           'owner': 'Hunlongyu',
