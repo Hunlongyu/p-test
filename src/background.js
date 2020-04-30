e3ea76e14f208a90afba7b5a08a4742512a741d5
hunlongyu <hunlongyu@gmail.com>
2020-04-30 17:11:23 +0800

    95

--- src/background.js
+++ src/background.js
@@ -36,7 +36,7 @@ function createWindow () {
     if (!process.env.IS_TEST) win.webContents.openDevTools()
   } else {
     win.loadURL('app://./index.html')
-    autoUpdater.setFeedURL('https://github.com/Hunlongyu/p-test/releases/latest')
+    autoUpdater.setFeedURL('https://github.com/Hunlongyu/p-test/releases/download/latest')
     autoUpdater.checkForUpdatesAndNotify()
   }
 
